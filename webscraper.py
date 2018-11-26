4f55524fa821d20e6c5f4411a0279aeb8d7e76e3
robert neuzil <robertneuzil@gmail.com>
2018-11-26 01:35:54 -0500

    links

--- webscraper.py
+++ webscraper.py
@@ -3,5 +3,15 @@ import requests
 
 source = requests.get('http://redditlist.com/').text
 
+
+# creates a BeautifulSoup object
 soup = BeautifulSoup(source, 'lxml')
-print(soup.prettify())
+'''
+links = soup.find_all('a')
+print(links)
+'''
+
+
+# inspect the page and find the html tags that you can pass to the soup object
+header = soup.find_all('a', class_= "sfw", target='_blank')
+print (header)
