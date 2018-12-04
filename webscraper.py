40f1fd06d522a17b831ba7b86a653c422285f860
robert neuzil <robertneuzil@gmail.com>
2018-12-04 17:35:01 -0500

    changed to a website designed to be scraped

--- webscraper.py
+++ webscraper.py
@@ -1,12 +1,18 @@
-import os
+from bs4 import BeautifulSoup
+import requests
+import sys
+import re
+'''
+A basic html page of  a ficitional book store that was designed to be tested with a webscraper
+'''
+x = 1
+source = requests.get(f'http://books.toscrape.com/catalogue/page-{x}.html').text
 
-# each website will create a new dir
+soup = BeautifulSoup(source, 'lxml')
 
-def create_project_dir(directory):
-	if not os.path.exists(directory):
-		print(f'Creating directory {directory}')
-		os.makedirs(directory)
-	else:
-		print ('that directory already exists')
+while x <= 5:
+	for display in soup.find_all(class_='product_pod'):
+		link = display.a.get('href')
+		print (f'The title of the book is \n "{display.h3.a.text}" \n while the link to the book is: \n http://books.toscrape.com/catalogue/{link} \n')
+	x += 1
 
-create_project_dir('robertest')
