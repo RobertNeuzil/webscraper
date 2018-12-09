66f008b17c7d1fc8e4238996f8c6aa9f075f3a04
robert neuzil <robertneuzil@gmail.com>
2018-12-09 01:43:30 -0500

    minor alteration

--- webscraper.py
+++ webscraper.py
@@ -5,18 +5,20 @@ import re
 '''
 A basic html page of  a ficitional book store that was designed to be tested with a webscraper
 '''
-x = 1
-source = requests.get(f'http://books.toscrape.com/catalogue/page-{x}.html').text
+def web_scraper(webadd)
+	page = 1
+	source = requests.get(f'{webadd}{page}.html').text
 
-soup = BeautifulSoup(source, 'lxml')
+	soup = BeautifulSoup(source, 'lxml')
 
-while x <= 20:
-	for display in soup.find_all(class_='product_pod'):
-		link = display.a.get('href')
-		
-		f = open('webscraper.txt','a')
-		f.write(f'\n The title of the book is \n "{display.h3.a.text}" \n while the link to the book is: \n http://books.toscrape.com/catalogue/{link} \n')
-		f.close()
-		#print (f'The title of the book is \n "{display.h3.a.text}" \n while the link to the book is: \n http://books.toscrape.com/catalogue/{link} \n')
-	x += 1
+	while page <= 20:
+		for display in soup.find_all(class_='product_pod'):
+			link = display.a.get('href')
+			
+			f = open('webscraper.txt','a')
+			f.write(f'\n The title of the book is \n "{display.h3.a.text}" \n while the link to the book is: \n http://books.toscrape.com/catalogue/{link} \n')
+			f.close()
+			#print (f'The title of the book is \n "{display.h3.a.text}" \n while the link to the book is: \n http://books.toscrape.com/catalogue/{link} \n')
+		page += 1
 
+web_scraper('http://books.toscrape.com/catalogue/page-')
